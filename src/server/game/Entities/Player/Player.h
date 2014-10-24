f4a84f4d7b78439b2f15e0d2b87d0c6788a9533e
lasyan3 <lasyan3@hotmail.com>
2014-10-24 21:28:12 +0200

    TODO : fix (DropQuestItems): compatibility fix for rev 56.

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1478,8 +1478,8 @@ class Player : public Unit, public GridObject<Player>
         bool HasPvPForcingQuest() const;
 
         // LASYAN3: AlwaysDropQuestItems
-        bool CanDropQuestItem(uint32 itemid); 
-        ObjectMgr::QuestMap GetAvailableQuestsForItem(uint32 itemid);
+		bool CanDropQuestItem(uint32 itemid) const;
+        ObjectMgr::QuestMap GetAvailableQuestsForItem(uint32 itemid) const;
 
         /*********************************************************/
         /***                   LOAD SYSTEM                     ***/
@@ -2311,9 +2311,6 @@ class Player : public Unit, public GridObject<Player>
 
         std::string GetMapAreaAndZoneString();
         std::string GetCoordsMapAreaAndZoneString();
-
-        std::string MsgQuestItemAdded;
-        int32 IdQuestItemAdded;
 
     protected:
         // Gamemaster whisper whitelist
